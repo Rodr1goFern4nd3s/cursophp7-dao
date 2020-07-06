0b3fef12a5cbd3cbea853212e9589b8d1d385345
Rodrigo Fernandes <fernandesrodrigo060@gmail.com>
2020-07-06 10:35:04 -0300

    Metodos de lista

--- index.php
+++ index.php
@@ -8,10 +8,23 @@ require_once("config.php");
 
 #echo json_encode($usuario);
 
+/*Carrega um usuário
 $root = new Usuario();
+$root->loadById(3);*/
+//echo $root;
 
-$root->loadById(3);
+//Carrega uma lista de usuário
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuarios buscando pelo login
+
+//$search = Usuario::search("ro");
+//echo json_encode($search);
+
+$usuario = new Usuario();
+$usuario->login("Marcos", "qwerty");
+
+echo $usuario;
 
 ?>
